035edbfe0745d32e00a8a7b22f652c6fddd2bce9
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-11 23:05:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ def get_english_meaning(load_address, emoticon)
   emo_hash = load_library(load_address)
   binding.pry 
   meaning_hash = emo_hash['get_meaning']
-  meaning_hash.each do |meaning, jap|
+  meaning_hash.each do |jap, meaning|
     ponja = meaning_hash.values
     if jap == emoticon
       return meaning
